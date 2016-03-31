ec0157b47eb2bde480993adf6f24be3b892cd536
lampon <670205545@qq.com>
2016-03-31 15:53:12 +0800

    添加了消息回复类

---
+++ WeChat.SDK/AdvancedAPIs/Message/Entities/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChat.SDK
+{
+    public class News
+    {
+        public string title { set; get; }
+        public string description { set; get; }
+        public string picurl { set; get; }
+        public string url { set; get; }
+    }
+}
